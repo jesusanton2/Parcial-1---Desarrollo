d7aa7128a08d05c1a9e5073eca97ccb26703dc98
jesusanton2 <jesusanton02@outlook.com>
2024-10-08 23:38:39 -0300

    first commit

---
+++ src/main/java/ADN/ParcialDesarrollo/ParcialDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package ADN.ParcialDesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialDesarrolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ADN/ParcialDesarrollo/ParcialDesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package ADN.ParcialDesarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialDesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
